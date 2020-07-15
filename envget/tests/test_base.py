05a637e0021d904d1a552138de4c4b5e4cf20109
Denis Anikin <ad@xfenix.ru>
2020-07-15 03:24:41 +0300

    Basic tests

--- envget/tests/test_base.py
+++ envget/tests/test_base.py
@@ -1,9 +1,31 @@
 """All tests gathers here.
 """
+from __future__ import annotations
+import decimal
+import pathlib
+import typing
+
+import pytest
+import faker
+
 from envget import base
 
 
-def test_parse_osenvparse() -> None:
+FAKE_GEN: faker.Faker = faker.Faker()
+FAKE_TYPES_MAP: dict = {
+    str: FAKE_GEN.pystr(),
+    int: FAKE_GEN.pyint(),
+    float: FAKE_GEN.pyfloat(),
+    bool: FAKE_GEN.pybool(),
+    decimal.Decimal: FAKE_GEN.pydecimal(),
+    pathlib.Path: pathlib.Path(),
+}
+
+
+@pytest.mark.parametrize("type_cast", FAKE_TYPES_MAP.keys())
+def test_parse_osenvparse(monkeypatch, type_cast) -> None:
     """Basic test.
     """
-    base.env("DEBUGME")
+    env_key: str = f"DEBUGME_KOKOK_PRIVET_{FAKE_GEN.pystr()}"
+    monkeypatch.setenv(env_key, str(FAKE_TYPES_MAP[type_cast]))
+    base.env(env_key, type_cast)
